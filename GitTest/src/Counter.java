d957b4355122340491e2921773b52f9c8a47140c
Vitalii <vitaliy.vasilishin2004@gmail.com>
2023-02-07 09:23:27 +0100

    last

--- GitTest/src/Counter.java
+++ GitTest/src/Counter.java
@@ -1,6 +1,6 @@
 public class Counter {
     public static void main(String[] args) {
-        for (int i = 0; i <15; i++) {
+        for (int i = 0; i <19; i++) {
 System.out.print(i+ " ");
         }
     }
